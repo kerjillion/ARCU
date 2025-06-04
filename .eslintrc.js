aa1f4abda6a26f9512ad96b1522ee5cabb336a57
raygions <raygions@gmail.com>
2025-06-04 16:48:33 -0400

    Shifted from Jasmin to Jest

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'prettier',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+  ignorePatterns: ['**/*.js', 'node_modules/', 'dist/', 'out-tsc/'],
+};
