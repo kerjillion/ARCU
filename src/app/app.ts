fa002b387b43a94ba108382d5109d32c1efd7ddc
raygions <raygions@gmail.com>
2025-06-05 10:16:53 -0400

    updating

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,60 +1,12 @@
-import { Component, signal, computed } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MaterialModule } from './material.module';
-import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { provideHttpClient, HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MaterialModule, ReactiveFormsModule, CommonModule],
-  // providers: [provideHttpClient()], // Moved to main.ts
-  templateUrl: './app.html',
-  styleUrls: ['./app.scss']
+  imports: [RouterOutlet],
+  template: `<router-outlet></router-outlet>`
 })
-export class AppComponent {
-  protected title = 'ARCU';
-
-  constructor(private http: HttpClient) {}
-
-  form = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.minLength(2)]),
-    email: new FormControl('', [Validators.required, Validators.email]),
-  });
-
-  onSubmit() {
-    if (this.form.valid) {
-      alert('Form submitted!\n' + JSON.stringify(this.form.value, null, 2));
-    }
-  }
-
-  fetchSampleData() {
-    this.http.get('https://jsonplaceholder.typicode.com/users/1').subscribe({
-      next: data => alert('Fetched user data:\n' + JSON.stringify(data, null, 2)),
-      error: err => alert('API error: ' + err.message)
-    });
-  }
-
-  theme = signal<'light' | 'dark'>('light');
-
-  toggleTheme() {
-    this.theme.update(t => t === 'light' ? 'dark' : 'light');
-    document.body.classList.toggle('dark-theme', this.theme() === 'dark');
-    document.body.classList.toggle('light-theme', this.theme() === 'light');
-  }
-
-  ngOnInit() {
-    document.body.classList.add('light-theme');
-  }
-
-  // Example: signal-based state for a counter
-  counter = signal(0);
-  doubleCounter = computed(() => this.counter() * 2);
-
-  increment() {
-    this.counter.update(c => c + 1);
-  }
-}
+export class AppComponent {}
 
 export { AppComponent as App };
---
+++ src/app/sample-presentation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-sample-presentation',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './sample-presentation.component.html',
+  styleUrls: ['./sample-presentation.component.scss']
+})
+export class SamplePresentationComponent {}
